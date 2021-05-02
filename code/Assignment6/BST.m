e69efb6361ef61c9188059a6f6c057ae7b99f4c1
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-05-02 00:35:37 -0400

    min, max, isEmpty

--- code/Assignment6/BST.m
+++ code/Assignment6/BST.m
@@ -18,6 +18,57 @@
     return self;
 }
 
+//start at root, and go left until a node doesn't have a left child. THat node is min
+-(int)min{ //wrapper
+    return [self recurMin:self.root];
+}
+
+-(int)recurMin:(ListNode *)node{
+    
+    //base case
+    if (node == nil){
+        return NO;
+    }
+    if (node.left == nil){
+        return node.object;
+    }
+    return [self recurMin:node.left];
+}
+//end of find min
+
+
+//start at root, go right until a node doesn't have a right child, has to be largest value
+-(int)max{
+    return [self recurMax:self.root];
+}
+
+-(int)recurMax:(ListNode *)node{
+    
+    //base case
+    if (node == nil){
+        return NO;
+    }
+    if(node.right == nil){
+        return node.object;
+    }
+    return [self recurMax:node.right];
+    
+}
+//end of max functions
+
+//if no root, is empty
+-(bool)isEmpty{
+    if(self.root == nil){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+
+
+
+
 //need to test this out first 
 -(void)put: (int)Key{
     if(self.root == nil){ // base
@@ -36,7 +87,7 @@
     
     //base for right side insert
     if(currentNode.object < Key && currentNode.right == nil){
-        currentNode.right = [[ListNode alloc] initWithObject:Key];
+        currentNode.right =  [[ListNode alloc] initWithObject:Key];
         //base for left side insert
     } else  if(currentNode.object > Key && currentNode.left == nil){
         currentNode.left = [[ListNode alloc] initWithObject:Key];
