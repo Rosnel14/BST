f4eaeb2d1b21bd8d279790a521893334f9eda87e
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-05-04 10:46:22 -0400

    init proj

---
+++ 21Questions/Questions21Game/Questions21Game/GameScene.h
@@ -0,0 +1,12 @@
+//
+//  GameScene.h
+//  Questions21Game
+//
+//  Created by Rosnel Leyva-Cortés on 5/4/21.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ 21Questions/Questions21Game/Questions21Game/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Questions21Game
+//
+//  Created by Rosnel Leyva-Cortés on 5/4/21.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
